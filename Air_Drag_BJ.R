5febc4f79d3a6549804018ba04f88e8339ea8767
b-jorges <bjoern_joerges@hotmail.de>
2020-03-22 20:42:18 -0400

    j

--- Air_Drag_BJ.R
+++ Air_Drag_BJ.R
@@ -33,6 +33,7 @@ setwd(Where_Am_I())
 source("Utilities/Funs.R")
 
 collapsed <- read.table(file = "Data/All_Data_Air_Drag.txt", header = T)
+Continuous <- read.table(file = "Data/ContinuousData.txt", header = T)
 
 air_drag <-  collapsed %>%
   select(trial,x_max,y_max,t_max,vx,
@@ -44,11 +45,13 @@ air_drag <-  collapsed %>%
   mutate(terror = rtime_timing-t_max-0.049,
          xerror = ball_x_spatial - x_max,
          OccludedDuration = t_max-visible,
+         OccludedPercentage = visible/t_max,
+         OccludedDistance = x_max-(x_max/2+vx*0.8*t_max*(OccludedPercentage-0.5)),
          # Differences with model modelling AD at oclusion
          xerror_model = ball_x_spatial - x_max_model,
          terror_model = rtime_timing-t_max_model-0.049,
          # Ratio Raw
-         xerrorratio = (x_max+xerror) / x_max,
+         xerrorratio = (OccludedDistance+xerror) / OccludedDistance,
          terrorratio = (OccludedDuration+terror) / OccludedDuration,
          # Ratio Model -
          xerror_ratio_model = xerror_model / x_max_model,
@@ -58,21 +61,21 @@ air_drag <-  collapsed %>%
          ball = factor(ball,levels = c("tennis","basket"),
                        labels = c("Tennis","Basket")),
          airdrag = case_when(air_drag == 1 ~ "Airdrag",
-                             air_drag == 0 ~ "No Airdrag"),
-  ) %>%
+                             air_drag == 0 ~ "No Airdrag"))
+2.8/3.5
+nAllTrials = length(air_drag$trial)
+
+air_drag = air_drag %>% 
   group_by(id,label) %>%
-  filter(terror > mean(terror)-2.5*sd(terror) & mean(terror)+2.5*sd(terror),
-         xerror > mean(xerror)-2.5*sd(xerror) & mean(xerror)+2.5*sd(xerror)) %>%
+  filter(terror > mean(terror)-2.5*sd(terror) & terror < mean(terror)+2.5*sd(terror),
+         xerror > mean(xerror)-2.5*sd(xerror) & xerror < mean(xerror)+2.5*sd(xerror)) %>%
   filter(trim(terror, filter = T)) %>%
   filter(trim(xerror, filter = T)) %>%
   mutate(sd_time = sd(terror),
          sd_space = sd(xerror))
 
-min(air_drag$OccludedDuration)
-max(air_drag$OccludedDuration)
-min(air_drag$terror)
-max(air_drag$terror)
-
+#number of excluded trials
+nAllTrials - length(air_drag$trial)
 
 #####Look at airdrag and stuff
 ggplot(air_drag[air_drag$cond_size == "Congruent",], aes(airdrag,terrorratio)) +
@@ -86,9 +89,9 @@ anova(H1_Temporal_lmer,H1_Temporal_lmer_NullModel)
 summary(H1_Temporal_lmer)
 
 H1_Temporal_lmer <- lmer(terrorratio-1 ~ 0 + (1|id), 
-                         data = air_drag[air_drag$airdrag == "Air_Drag",])
+                         data = air_drag[air_drag$airdrag == "Airdrag",])
 H1_Temporal_lmer_NullModel <- lmer(terrorratio-1 ~ (1|id), 
-                                   data = air_drag[air_drag$airdrag == "Air_Drag",])
+                         data = air_drag[air_drag$airdrag == "Airdrag",])
 anova(H1_Temporal_lmer,H1_Temporal_lmer_NullModel)
 summary(H1_Temporal_lmer_NullModel)
 
@@ -98,6 +101,8 @@ H1_Temporal_lmer_NullModel <- lmer(terrorratio-1 ~ (1|id),
                                    data = air_drag[air_drag$airdrag == "No Airdrag",])
 anova(H1_Temporal_lmer,H1_Temporal_lmer_NullModel)
 summary(H1_Temporal_lmer_NullModel)
+
+
 
 get_prior(terrorratio ~ airdrag + (1|id),
           data = air_drag, family = gaussian())
@@ -122,38 +127,134 @@ Hypotheses = hypothesis(fit2,c("airdragNoAirdrag < 0",
                                "Intercept = 1",
                                "Intercept+airdragNoAirdrag = 1"))
 
+ggplot(air_drag, aes(as.factor(cond_size),t_max_model-t_max, color = as.factor(air_drag))) +
+  geom_point() +
+  facet_grid(.~ball)
 
-ggplot(air_drag, aes(air_drag,xerror_ratio)) +
-  geom_violin()
-H1_Spatial_TestModel <- lmer(xerror_ratio ~ air_drag + (1|id), 
+
+H1_Spatial_TestModel <- lmer(xerrorratio ~ airdrag + (1|id), 
                          data = air_drag)
-H1_Spatial_NullModel <- lmer(xerror_ratio ~ (1|id), 
+H1_Spatial_NullModel <- lmer(xerrorratio ~ (1|id), 
                                    data = air_drag)
 anova(H1_Spatial_TestModel,H1_Spatial_NullModel)
 summary(H1_Spatial_TestModel)
+
+H1_Temporal_lmer <- lmer(xerrorratio-1 ~ 0 + (1|id), 
+                         data = air_drag[air_drag$airdrag == "Airdrag",])
+H1_Temporal_lmer_NullModel <- lmer(xerrorratio-1 ~ (1|id), 
+                                   data = air_drag[air_drag$airdrag == "Airdrag",])
+anova(H1_Temporal_lmer,H1_Temporal_lmer_NullModel)
+summary(H1_Temporal_lmer_NullModel)
+
+H1_Temporal_lmer <- lmer(xerrorratio-1 ~ 0 + (1|id), 
+                         data = air_drag[air_drag$airdrag == "No Airdrag",])
+H1_Temporal_lmer_NullModel <- lmer(xerrorratio-1 ~ (1|id), 
+                                   data = air_drag[air_drag$airdrag == "No Airdrag",])
+anova(H1_Temporal_lmer,H1_Temporal_lmer_NullModel)
+summary(H1_Temporal_lmer_NullModel)
+
+
+ggplot(air_drag,aes(terrorratio,xerrorratio, color = cond_size)) +
+  geom_point(alpha=0.2) +
+  coord_fixed() +
+  geom_smooth() +
+  xlim(c(0,2))
 
 
 ######Look at ball size and congruency
 ##time
 ggplot(air_drag, aes(cond_size,terror_ratio, color = ball)) +
-  geom_violin()
-SizeCongruency_TestModel_Time <- lmer(terror_ratio ~ ball*cond_size + (1|id), 
-                         data = air_drag)
-SizeCongruency_NullModel_Time <- lmer(terror_ratio ~ ball + cond_size + (1|id), 
-                                   data = air_drag)
+  geom_violin() +
+  binomial_smooth()
+
+
+SizeCongruency_TestModel_Time <- lmer(terrorratio ~ ball*cond_size + (1|id), 
+                         data = air_drag[air_drag$airdrag == "Airdrag",])
+SizeCongruency_NullModel_Time <- lmer(terrorratio ~ ball + cond_size + (1|id), 
+                                      data = air_drag[air_drag$airdrag == "Airdrag",])
 anova(SizeCongruency_TestModel_Time,SizeCongruency_NullModel_Time)
+summary(SizeCongruency_TestModel_Time)
 #(lol) (jaja)
 
 ##space
-ggplot(air_drag, aes(cond_size,xerror_ratio, color = ball)) +
-  geom_violin()
+ggplot(air_drag, aes(ball,xerrorratio, color = cond_size)) +
+  geom_violin() +
+  geom_boxplot()
 SizeCongruency_TestModel_Space <- lmer(xerror_ratio ~ ball*cond_size + (1|id), 
-                                      data = air_drag)
+                                       data = air_drag[air_drag$airdrag == "Airdrag",])
 SizeCongruency_NullModel_Space <- lmer(xerror_ratio ~ ball + cond_size + (1|id), 
-                                      data = air_drag)
+                                       data = air_drag[air_drag$airdrag == "Airdrag",])
 anova(SizeCongruency_TestModel_Space,SizeCongruency_NullModel_Space)
 summary(SizeCongruency_TestModel_Space)
 #interesting
+
+
+
+
+#####Hypothesis 1
+#Hypothesis 1a
+fit1 <- brm(bf(terrorratio ~ airdrag + (1|id),
+               sigma ~ airdrag + (1|id)),
+            data = air_drag, family = gaussian())
+
+Hypotheses = hypothesis(fit1,c("airdragNoAirdrag < 0",
+                               "abs(Intercept-1) < abs(Intercept+airdragNoAirdrag-1)"))
+
+#Hypothesis 1a
+fit2 <- brm(bf(xerrorratio ~ airdrag + (1|id),
+               sigma ~ airdrag + (1|id)),
+            data = air_drag, family = gaussian())
+
+Hypotheses = hypothesis(fit2,c("airdragNoAirdrag < 0",
+                               "abs(Intercept-1) < abs(Intercept+airdragNoAirdrag-1)"))
+
+
+
+#####Hypothesis 2
+#Hypothesis 2a
+#
+fit3 <- brm(bf(terrorratio ~ ball + condsize + (1|id),
+               sigma ~ ball + condsize + (1|id)),
+            data = air_drag, family = gaussian())
+
+Hypotheses = hypothesis(fit3,c("sigma_ballBasket > 0",
+                               "sigma_condsizeIncongruent < 0",
+                               "sigma_ballBasket:condsizeIncongruent < 0"))
+
+fit4 <- brm(bf(xerrorratio ~ ball*condsize + (1|id),
+               sigma ~ ball*condsize + (1|id)),
+            data = air_drag, family = gaussian())
+Hypotheses = hypothesis(fit4,c("sigma_ballBasket > 0",
+                               "sigma_condsizeIncongruent < 0",
+                               "sigma_ballBasket:condsizeIncongruent < 0"))
+
+fit5 <- brm(bf(terrorratio ~ condsize + (1|id),
+               sigma ~ condsize + (1|id)),
+            data = air_drag[air_drag$airdrag == "Airdrag",], family = gaussian())
+Hypotheses = hypothesis(fit5,c("sigma_condsizeIncongruent < 0"))
+
+
+fit6 <- brm(bf(xerrorratio ~ condsize + (1|id),
+               sigma ~ condsize + (1|id)),
+            data = air_drag[air_drag$airdrag == "Airdrag",], family = gaussian())
+Hypotheses = hypothesis(fit6,c("sigma_condsizeIncongruent < 0"))
+
+fit7 <- brm(bf(terrorratio ~ ball + (1|id),
+               sigma ~ ball + (1|id)),
+            data = air_drag, family = gaussian())
+Hypotheses = hypothesis(fit7,c("sigma_ballTennis < 0"))
+
+fit8 <- brm(bf(xerrorratio ~ ball + (1|id),
+               sigma ~ ball + (1|id)),
+            data = air_drag, family = gaussian())
+Hypotheses = hypothesis(fit8,c("sigma_ballTennis < 0"))
+
+
+######exploratory impact of air drag on
+Hypotheses = hypothesis(fit1,c("sigma_airdragNoAirdrag > 0"))
+Hypotheses = hypothesis(fit2,c("sigma_airdragNoAirdrag > 0"))
+
+
 
 air_drag %>%
   group_by(ball,cond_size) %>%
@@ -214,3 +315,23 @@ dat <- as.data.frame(fit)
 hypothesis(dat, "b_age > 0")
 
 ## End(Not run)
+roh = 1.225
+cd = 0.535
+r = 0.12
+D = 0.5 * roh * cd * pi * r^2
+m = 0.6
+vx = 3.5
+vy = 6.86
+g = 9.81
+
+for (i in seq(0,1.4*0.575,0.01)){
+  vt = (vx^2+vy^2)^0.5
+  ax = -(D/m)*vt*vx
+  ay = g - (D/m)*vt*vy
+  
+  vx = vx  + ax  * 0.01
+  vy = vy + ay  * 0.01
+  
+  print(i)
+}
+
